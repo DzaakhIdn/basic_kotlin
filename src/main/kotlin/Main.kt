8f39f66fbf0207cc2cd59a3007d1c599bb184b55
Abdurrahman Dzakiy <143054189+DzaakhIdn@users.noreply.github.com>
2024-10-07 19:35:13 +0700

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,69 @@
 package org.belajar.kotlin
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun main(){
+    val name = "Dzaki" //Datanya tidak bisa dirubah kayak const
+    var umur = 17 //Datanya dapat berubah
+    umur = 18
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val tinggal: String? = null
+    val textLenght = tinggal?.length?:"Tidak tahu" //Elvis Operator atau memberikan nilai default pada null
+    println("nama saya $name umur saya $umur dan saya tinggal di $textLenght")
+
+//    Array
+    val randomList = mutableListOf("ba", 1, true)
+    randomList[2] = false
+    randomList.add("hayo")
+    randomList.add(1,"Papo")
+    println(randomList)
+
+//    Operator Matematika
+    var a = 10
+    val b = 14
+
+    val math = a * b
+    println("hasil dari $a + $b adalah $math")
+    println("tambah " + a++)
+    println("tambah " + a++)
+
+    val textName = "Abdurrahman Dzaki"
+    val charIndex = textName[5]
+
+    println("Huruf ke 6 dari $textName adalah $charIndex")
+
+    val openHour = 7
+    val closeHour = 10
+    val hourNow = 8
+    val status: String
+
+    if (hourNow > openHour){
+        status = "Alredy open"
+    }else if (openHour == hourNow){
+        status = "wait a minute, will be open"
     }
+    else{
+        status = "Alredy close"
+    }
+    println("The store is $status")
+
+    val isOpen = if (hourNow >= openHour && hourNow <= closeHour){
+        true
+    }else{
+        false
+    }
+
+    println(isOpen)
+
+    val notOpen = if (!isOpen){
+        println("tutup")
+    } else {
+        println("buka")
+    }
+
+    println(notOpen)
+
+    val stringInt = "23"
+    val intVal = 20
+
+    println(stringInt.toInt() + intVal)
 }
